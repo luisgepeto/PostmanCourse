3b19030ba5062439e09d8b90ed024e7687ffdc94
luisgepeto <luisgepeto@gmail.com>
2017-02-26 21:05:40 -0600

    added files

---
+++ PostmanApi/Controllers/BasicController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostmanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class BasicController : Controller
+    {
+
+        [HttpGet("simple")]
+        public string Simple()
+        {
+            return "This is a simple result";
+        }
+
+    }
+}
